711c408216acd9682c37e442831c19f3e294794a
eluck <mail@eluck.me>
2015-11-01 01:52:21 +0300

    Make the polyfill to be a polyfill

--- nexty-polyfill.js
+++ nexty-polyfill.js
@@ -1,17 +1,35 @@
-(function () {
+(function (window) {
+  window.presentationEngine = {};
+
   function getEngine() {
-    if (window.shower) return {engine: 'shower', version: undefined};
-    return {};
+    if (window.shower) return {name: 'shower', version: shower.version};  //shower.version is currently undefined
+    return {error: 'presentation engine is not recognized'};
   }
 
+  function fillPolyFill() {
+    presentationEngine.info = getEngine();
+    if (!presentationEngine.info.name) return;
+
+    if (presentationEngine.info.name === 'shower' && !presentationEngine.info.version) {
+      presentationEngine.next = shower._turnNextSlide;
+      presentationEngine.prev = shower._turnPreviousSlide;
+      presentationEngine.zoomIn = shower.enterSlideMode;
+      presentationEngine.zoomOut = shower.enterListMode;
+    }
+  }
+
+  fillPolyFill();
+
   window.addEventListener('message', function(event) {
+    if (event.data === 'nexty-get-engine') return window.parent.postMessage(presentationEngine.info, '*');
+    if (!presentationEngine.info.name) return window.parent.postMessage('error: ' + presentationEngine.info.error, '*');
     switch (event.data) {
-      case 'nexty-get-engine': return window.parent.postMessage(getEngine(), '*');
-      case 'nexty-next': return shower._turnNextSlide();
-      case 'nexty-prev': return shower._turnPreviousSlide();
-      case 'nexty-zoomIn': return shower.enterSlideMode();
-      case 'nexty-zoomOut': return shower.enterListMode();
-      default: return window.parent.postMessage('unrecognized command', '*');
+      case 'nexty-next': return presentationEngine.next();
+      case 'nexty-prev': return presentationEngine.prev();
+      case 'nexty-zoomIn': return presentationEngine.zoomIn();
+      case 'nexty-zoomOut': return presentationEngine.zoomOut();
+      default: return window.parent.postMessage('error: unrecognized command', '*');
     }
   });
-})();
+
+})(window);
