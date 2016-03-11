ed4bebfc44736faf80a1d154800193a53c0f314e
eluck <mail@eluck.me>
2016-03-11 17:34:34 +0300

    Add flowtime presentation engine support

--- nexty-polyfill.js
+++ nexty-polyfill.js
@@ -5,6 +5,7 @@
     if (window.shower) return {name: 'shower', version: shower.version};  //shower.version is currently undefined
     if (window.Reveal) return {name: 'reveal', version: Reveal.version};  //Reveal.version is currently undefined
     if (window.$ && $.deck) return {name: 'deck', version: $.deck.version}; //$.deck.version is currently undefined
+    if (window.Flowtime) return {name: 'flowtime', version: Flowtime.version};  //Reveal.version is currently undefined
     return {error: 'presentation engine is not recognized'};
   }
 
@@ -39,6 +40,15 @@
       presentationEngine.zoomOut = function() {};
       presentationEngine.last = function() {$.deck('go', $.deck('getSlides').length - 1);};
       presentationEngine.first = $.deck.bind($.deck, 'go', 0);
+    }
+
+    if (presentationEngine.info.name === 'flowtime' && !presentationEngine.info.version) {
+      presentationEngine.next = Flowtime.nextFragment;
+      presentationEngine.prev = Flowtime.prevFragment;
+      presentationEngine.zoomIn = Flowtime.showOverview.bind(Flowtime, false);
+      presentationEngine.zoomOut = Flowtime.showOverview.bind(Flowtime, true);
+      presentationEngine.last = Flowtime.gotoEnd;
+      presentationEngine.first = Flowtime.gotoHome;
     }
   }
 
